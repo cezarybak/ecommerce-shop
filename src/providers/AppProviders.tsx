89da01a7ad035b4a0230d3dcd286e47037786121
cezarybak <cezary.bak0@gmail.com>
2022-04-23 20:44:03 +0200

    feat: deploy

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -2,5 +2,5 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { AppProvidersProps } from "./AppProviders.types";
 
 export const AppProviders = ({ children }: AppProvidersProps) => (
-  <Router>{children}</Router>
+  <Router basename={process.env.PUBLIC_URL}>{children}</Router>
 );
